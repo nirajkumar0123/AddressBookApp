63ed83aaca20154a858ccf3cd263d403b87da25f
Niraj Kumar <nirajgupta7481@gmail.com>
2025-03-01 13:42:52 +0530

    [Niraj] Add . added lombok library  to auto generate getters and setters

--- src/main/java/com/bridgelabz/addressbookapp/model/AddressBook.java
+++ src/main/java/com/bridgelabz/addressbookapp/model/AddressBook.java
@@ -4,12 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 @Entity
-@Getter
-@Setter
+@Data
 public class AddressBook {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,10 +15,4 @@ public class AddressBook {
     private String name;
     private String address;
     private String phoneNumber;
-//
-//    public AddressBook(String name, String address, String phoneNumber) {
-//        this.name = name;
-//        this.address = address;
-//        this.phoneNumber = phoneNumber;
-//    }
 }
